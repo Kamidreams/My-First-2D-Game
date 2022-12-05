881115a25058dbfa8fd2a193a00a03a1ac518912
Kamidreams <105478024+Kamidreams@users.noreply.github.com>
2022-12-05 11:32:41 -0800

    Capy game map and player and collectible

--- My First 2D Game/Assets/Scripts/Player.cs
+++ My First 2D Game/Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 10;
+    private Rigidbody2D _playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
     }
 }
