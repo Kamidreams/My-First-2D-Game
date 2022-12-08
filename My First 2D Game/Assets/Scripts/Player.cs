b2acad0cf261e20ee0dabfe9973c44e6d75c2808
Kamidreams <105478024+Kamidreams@users.noreply.github.com>
2022-12-08 11:05:14 -0800

    Fixing Player Movement

--- My First 2D Game/Assets/Scripts/Player.cs
+++ My First 2D Game/Assets/Scripts/Player.cs
@@ -6,11 +6,11 @@ public class Player : MonoBehaviour
 {
     public float speed = 10;
      public float jumpForce = 10;
+     public float xRange;
     public bool isOnGround = false;
-    public bool isGameOver = false;
-    public AudioClip jumpSound;   
+    public bool isGameOver = false; 
+    private float _horizontalInput;
     private Animator _playerAnim;
-    private AudioSource _playerAudio;
     private Collider2D _myCollider;
     private Rigidbody2D _playerRb;
 
@@ -18,7 +18,6 @@ public class Player : MonoBehaviour
     void Start()
     {
         _playerAnim = GetComponent<Animator>();
-        _playerAudio = GetComponent<AudioSource>();
         _playerRb = GetComponent<Rigidbody2D>();
         _myCollider = GetComponent<Collider2D>();
     }
@@ -31,10 +30,21 @@ public class Player : MonoBehaviour
 
     }
 
-    void PlayerMovement()
+   void PlayerMovement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        _playerRb.velocity = new Vector2(horizontalInput = speed, _playerRb.velocity.y);
+         _horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * _horizontalInput * speed * Time.deltaTime);
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
     }
     void FlipSprite()
     {
